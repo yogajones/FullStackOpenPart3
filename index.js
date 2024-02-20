477ed5e49a0e63386c37e289f56c248abf4500db
yogajones <95644662+yogajones@users.noreply.github.com>
2024-02-20 17:52:35 +0200

    Ex 3.8

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('request-body', (request, response) => JSON.stringify(request.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'));
 
 let persons = [
     {

1a2f35f45248ffae8412c2ade896d11a0bdbbc7f
yogajones <95644662+yogajones@users.noreply.github.com>
2024-02-20 16:00:26 +0200

    Ex 3.2

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const http = require('http')
-
 const express = require('express')
 const app = express()
 
@@ -28,6 +26,14 @@ let persons = [
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const date = new Date().toString()
+    response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${date}</p>
+        `)
 })
 
 const PORT = 3001

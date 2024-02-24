5171e8697d3a785808d4b91b9e228b6f3a76c06d
yogajones <95644662+yogajones@users.noreply.github.com>
2024-02-24 12:53:52 +0200

    Ex 3.11

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 morgan.token('request-body', (request, response) => JSON.stringify(request.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'));
 

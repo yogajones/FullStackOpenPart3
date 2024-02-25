69a4dabc43960e1383da8f680d829a2d4cfe4424
yogajones <95644662+yogajones@users.noreply.github.com>
2024-02-25 15:40:32 +0200

    Ex 3.15

--- index.js
+++ index.js
@@ -89,10 +89,10 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
 })
 
 const PORT = process.env.PORT

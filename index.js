db49e2061b24b91b82e8259c138b4b3cfae198bf
yogajones <95644662+yogajones@users.noreply.github.com>
2024-02-25 16:49:55 +0200

    Ex 3.16

--- index.js
+++ index.js
@@ -11,36 +11,15 @@ app.use(express.static('dist'))
 morgan.token('request-body', (request, response) => JSON.stringify(request.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'));
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5423523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary P",
-        number: "39-23-6423122"
-    }
-]
 
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(people => {
-        response.json(people)
-    })
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+        .then(people => { response.json(people) })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+// fix this in Ex 3.18 with proper error handling
+/*app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
 
@@ -49,51 +28,63 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
-})
+})*/
 
 app.get('/info', (request, response) => {
     const date = new Date().toString()
+    peopleCount = 1 // fix this in Ex 3.18
     response.send(`
-        <p>Phonebook has info for ${persons.length} people</p>
+        <p>Phonebook has info for ${peopleCount} people</p>
         <p>${date}</p>
         `)
 })
 
-app.post('/api/persons', (request, response) => {
-    /*const nameExists = (name) => {
-        return persons.find(person => person.name === name)
-    }*/
-
-    const body = request.body
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'name or number cannot be empty'
-        })
-    }
-    /*if (nameExists(body.name)) {
-        return response.status(400).json({
-            error: 'a person with this name already exists'
-        })
-    }*/
-
+app.post('/api/persons', (request, response, next) => {
     const newPerson = new Person({
-        name: body.name,
-        number: body.number
+        name: request.body.name,
+        number: request.body.number
     })
+
+    if (!request.body.name || !request.body.number) {
+        const error = new Error('Name or number cannot be empty')
+        error.status = 400
+        return next(error)
+    }
 
     console.log(`Adding new person: ${newPerson.name}..`)
-    newPerson.save().then(savedPerson => {
-        console.log(`..${savedPerson.name} added!`)
-        response.json(savedPerson)
-    })
+    newPerson.save()
+        .then(savedPerson => {
+            console.log(`..${savedPerson.name} added!`)
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
         .then(result => {
             response.status(204).end()
         })
+        .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: "Check the format of the person's ID" })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
